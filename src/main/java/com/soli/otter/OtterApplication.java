4e90479dc4a7a28cfdc70377d354716cc7cbee8a
consoli.log <eumsoli@gmail.com>
2024-12-12 02:02:30 +0900

    🎉 Begin OTTer Project!

---
+++ src/main/java/com/soli/otter/OtterApplication.java
@@ -0,0 +1,17 @@
+package com.soli.otter;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtterApplication {
+
+	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		SpringApplication.run(OtterApplication.class, args);
+	}
+}
